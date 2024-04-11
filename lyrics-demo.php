af146e5414de46c17083cdabc854d58a5d4c9a64
TSF Anonwins <lykos92@gmail.com>
2024-04-11 05:29:49 +0300

    Update lyrics-demo.php

--- lyrics-demo.php
+++ lyrics-demo.php
@@ -2,14 +2,18 @@
 
 $query = "quicksilver - don't cry my lady love";
 
-// Google Search API Key (free limit is 100 requests/day. Minimum 1 key)
+// Google Search API Keys (free limit is 100 requests/day. Minimum 1 key)
+// Get API key from https://developers.google.com/custom-search/v1/introduction 
+// (Click 'Get A Key')
 $google_api_keys = [
-    'AIzaSyCQ6LZGhv9fcd-v3Z2qlV1VP273GLtw', // example
-    'AIzaSyDDppvfDbQH9MyNUWNBf_CKTd240_g1Y', // example
+    '__YOUR_GOOGLE_SEARCH_API_KEY_1__',
+    '__YOUR_GOOGLE_SEARCH_API_KEY_2__',
 ];
 
 // Google Custom Search Engine CX
-$google_cx = 'd67a8a54c4f40cf'; // example
+// Create from here: https://programmablesearchengine.google.com/controlpanel/all
+// Click 'Add' and include only URLs that start with www.azlyrics.com/lyrics/*
+$google_cx = '__YOUR_GOOGLE_SEARCH_ENGINE_CX__';
 
 // Initialize finder
 require_once(__DIR__.'/class.azlyricsfind.php');
